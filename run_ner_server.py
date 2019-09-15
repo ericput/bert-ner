1557b2aa82b55c6b18177db33e6c49ac1945e6bd
putong <putong7@outlook.com>
2019-09-15 22:38:59 +0800

    Add some notes

--- run_ner_server.py
+++ run_ner_server.py
@@ -23,3 +23,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     model = NJUNER(model_dir=args.model_dir, batch_size=args.batch_size)
     start(host=args.ip, port=args.port, debug=args.debug)
+
+'''
+上面的代码使用Flask库架设NER REST服务器（需要先安装pip包njuner），服务器地址为http://ip:port/ner，服务方法为POST。
+数据请求格式为{'text': str|list<str>}；数据回复格式为list<lineResult>，其中lineResult格式为list<[token_tag, token]>。
+
+使用例子：
+服务器端：
+    在ip为111.112.113.114的服务器上运行'python run_ner_server.py --model_dir model_msra'
+客户端：
+    import requests
+    r = requests.post(url='http://ip:port/ner', json={'text': '李雷和韩梅梅去上海迪斯尼乐园玩。'})
+    print(r.json())
+    '[[['B-PER', '李'], ['I-PER', '雷'], ['O', '和'], ['B-PER', '韩'], ['I-PER', '梅'], ['I-PER', '梅'], ['O', '去'], 
+    ['B-ORG', '上'], ['I-ORG', '海'], ['I-ORG', '迪'], ['I-ORG', '斯'], ['I-ORG', '尼'], ['I-ORG', '乐'], ['I-ORG', '园'], 
+    ['O', '玩'], ['O', '。']]]'
+'''
